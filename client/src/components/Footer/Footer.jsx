c3ad0bc885129e5dd043f5552772ea65eb868d97
Iva <iva_dkostadinova@abv.bg>
2024-07-24 13:16:47 +0300

    Create Footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return (
+        <footer>
+        <div>
+            <h4>Have fun</h4>
+            <p>Design by Iva Kostadinova &#169;</p>
+        </div>
+    </footer>
+    )
+}
