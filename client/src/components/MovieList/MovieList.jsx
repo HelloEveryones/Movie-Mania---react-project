4c35579ce90385078fead047c3e2925f6546c3d1
Iva <iva_dkostadinova@abv.bg>
2024-07-24 13:22:27 +0300

    Create MolvieList component

---
+++ client/src/components/MovieList/MovieList.jsx
@@ -0,0 +1,45 @@
+export default function MovieList(){
+    return (
+        <div className="movie-list">
+        <h1>All Movies</h1>
+        <ul>
+            <li>
+                <h3>Star Wars</h3>
+                <img src="./static/images/mbr-215x146.jpg" alt="star wars image"/>
+                <h5>Steven Speelberg <span>2005</span></h5>
+                <p>Fantasy</p>
+                <button>Details</button>
+            </li>
+            <li>
+                <h3>Star Wars</h3>
+                <img src="./static/images/mbr-215x146.jpg" alt="star wars image"/>
+                <h5>Steven Speelberg <span>2005</span></h5>
+                <p>Fantasy</p>
+                <button>Details</button>
+            </li>
+            <li>
+                <h3>Star Wars</h3>
+                <img src="./static/images/mbr-215x146.jpg" alt="star wars image"/>
+                <h5>Steven Speelberg <span>2005</span></h5>
+                <p>Fantasy</p>
+                <button>Details</button>
+            </li>
+            <li>
+                <h3>Star Wars</h3>
+                <img src="./static/images/mbr-215x146.jpg" alt="star wars image"/>
+                <h5>Steven Speelberg <span>2005</span></h5>
+                <p>Fantasy</p>
+                <button>Details</button>
+            </li>
+            <li>
+                <h3>Star Wars</h3>
+                <img src="./static/images/mbr-215x146.jpg" alt="star wars image"/>
+                <h5>Steven Speelberg <span>2005</span></h5>
+                <p>Fantasy</p>
+                <button>Details</button>
+            </li>
+
+        </ul>
+    </div>
+    )
+}
